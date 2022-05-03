83858ff5fc4e51fd74898c9a1d967df0d47e963e
jrotty <bssf@qq.com>
2022-05-03 14:51:29 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -18,7 +18,7 @@ use Utils\Helper;
  * typecho 评论通过时发送邮件提醒,要求typecho1.2.0及以上,项目地址<a href="https://github.com/jrotty/CommentNotifier" target="_blank">https://github.com/jrotty/CommentNotifier</a>
  * @package CommentNotifier
  * @author 泽泽社长
- * @version 1.2.7
+ * @version 1.2.8
  * @link http://blog.zezeshe.com
  */
 
