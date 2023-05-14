9bcf037421c568b4a3fc4a3da1010920c5ad8a5d
jrotty <bssf@qq.com>
2023-05-14 14:05:25 +0800

    增强表情尺寸控制的兼容性

--- Plugin.php
+++ Plugin.php
@@ -588,10 +588,8 @@ if($("#tuisongtype :radio:checked").val()=='aliyun')
         
         $style='style="display: inline-block;vertical-align: bottom;margin: 0;" width="30"';//限制表情尺寸
         
-        $commentText=str_replace('class="biaoqing"',$style,$commentText);
-        $Ptext=str_replace('class="biaoqing"',$style,$Ptext);
-        $commentText=str_replace('data-emoji',$style,$commentText);
-        $Ptext=str_replace('data-emoji',$style,$Ptext);
+        $commentText=str_replace('class="biaoqing',$style.' class="biaoqing',$commentText);
+        $Ptext=str_replace('class="biaoqing',$style.' class="biaoqing',$Ptext);
         
         
         $content = self::getTemplate($html);
