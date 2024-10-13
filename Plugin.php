f2359d29be8e5d5eefe4be326ca750007b6bed63
jrotty <bssf@qq.com>
2024-10-13 10:26:10 +0800

    Update ver

--- Plugin.php
+++ Plugin.php
@@ -25,7 +25,7 @@ require dirname(__FILE__) . '/PHPMailer/Exception.php';
  * 
  * @package CommentNotifier
  * @author 泽泽社长
- * @version 1.7.0
+ * @version 1.7.1
  * @link https://github.com/jrotty/CommentNotifier
  */
 
