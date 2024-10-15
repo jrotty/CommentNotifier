78cd63ab6b3239986a8ab9628724b4b3f0750b6a
jrotty <bssf@qq.com>
2024-10-15 15:55:50 +0800

    Update config.php

--- zemailapi/config.php
+++ zemailapi/config.php
@@ -1,6 +1,6 @@
 <?php
 $info=array(
-    'auth'=>'',//api秘钥，设置好后，你的api地址就是：https://域名/zemailapi/?auth=n你得秘钥地址
+    'auth'=>'',//api秘钥，设置好后，你的api地址就是：https://域名/zemailapi/?auth=你得秘钥地址
     'Host' =>'smtp.qq.com', // SMTP 服务地址,QQ邮箱为：smtp.qq.com
     'Username' =>'xxxx@qq.com',// SMTP 用户名一般就是邮箱地址
     'Password'=>'',// SMTP 密码
