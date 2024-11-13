5053245f025592bc9b45cccd6af6dc8025e5db06
jrotty <bssf@qq.com>
2024-11-13 11:30:25 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -21,7 +21,7 @@ require dirname(__FILE__) . '/PHPMailer/SMTP.php';
 require dirname(__FILE__) . '/PHPMailer/Exception.php';
 
 /**
- * typecho评论通过时发送邮件提醒,要求typecho1.2.0及以上<br>其他付费产品：https://store.typecho.work/
+ * typecho 评论通过时发送邮件提醒,要求typecho1.2.0及以上<br>其他付费产品：<a href="https://store.typecho.work/"target="_blank" rel="noopener noreferrer">https://store.typecho.work/</a>
  * 
  * @package CommentNotifier
  * @author 泽泽社长
